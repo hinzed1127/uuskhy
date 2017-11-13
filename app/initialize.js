c4b0a99f14091f11e6a457dfddf14d385a3969fb
Dan Hinze <hinzed1127@gmail.com>
2017-11-12 21:42:52 -0600

    Initial scaffolding, add images

---
+++ app/initialize.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+	// do your setup here
+	var $ = require('jquery');
+	console.log('Tasty Brunch, just trying to use jQuery!', $('body'));
+
+	console.log('Initialized app');
+});
